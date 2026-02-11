9d95d8b4c99fbbeb71c3dda2de4fc301a7ae0054
Vladimir Litvinchik <vladimir.litvinchik@aspose.com>
2026-02-11 11:16:48 +0200

    Use consolidated config.toml from common submodule

--- build_docs.cmd
+++ build_docs.cmd
@@ -5,4 +5,4 @@ call git submodule foreach git pull origin master
 xcopy net common\content\%PRODUCT%\net /s /e /Y /i
 xcopy _index.md common\content\%PRODUCT% 
 cd common
-call hugo server --config config-geekdoc.toml,ignore-total-config.toml,show-feedback-config.toml
+call hugo server
--- build_docs.sh
+++ build_docs.sh
@@ -5,4 +5,4 @@ git submodule foreach git pull origin master
 mkdir -p common/content/$PRODUCT/net && cp -r net common/content/$PRODUCT/
 cp -r _index.md common/content/$PRODUCT
 cd common
-hugo server --config config-geekdoc.toml,ignore-total-config.toml,show-feedback-config.toml
+hugo server
